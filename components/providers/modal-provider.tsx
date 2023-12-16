7bd02d2e805706dc0453d20271f0e8697c04e8d5
vadzola <vadzola@yahoo.com.hk>
2023-12-16 15:17:44 +0100

    final

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { useEffect, useState } from "react";
+import { InviveModal } from "@/components/modals/invite-modals";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
+import { DeleteChannelModal } from "../modals/delete-channel-modal";
+import { EditChannelModal } from "../modals/edit-channel-modal";
+import { MessageFileModal } from "../modals/message-file-modal";
+import { DeleteMessageModal } from "../modals/delete-message-modal";
+
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <CreateServerModal />
+      <InviveModal />
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal/>
+      <MessageFileModal />
+      <DeleteMessageModal />
+      
+    </>
+  );
+};
+
+export default ModalProvider;
---
+++ next.config.js
@@ -0,0 +1,17 @@
+const { buffer } = require("stream/consumers");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+  },
+};
+
+module.exports = nextConfig;
